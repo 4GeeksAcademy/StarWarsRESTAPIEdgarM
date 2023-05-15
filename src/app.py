cf250ef7a28e586498b53f07430aff7e3f36a7fa
EdgarDMen <123202799+EdgarDMen@users.noreply.github.com>
2023-05-15 20:27:22 +0000

    original changes

--- src/app.py
+++ src/app.py
@@ -1,51 +1,78 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+from flask import Flask, jsonify, request
+from models import db, User, People, Planet
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-MIGRATE = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Or the URI of your choice
 db.init_app(app)
-CORS(app)
-setup_admin(app)
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+@app.route('/people', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    return jsonify([p.name for p in all_people])
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify(person.name)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planet.query.all()
+    return jsonify([p.name for p in all_planets])
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify(planet.name)
 
-    return jsonify(response_body), 200
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    return jsonify([u.username for u in all_users])
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    user = User.query.get_or_404(request.args.get('user_id'))  # Assuming user_id is passed as a query param
+    return jsonify({
+        'favorite_people': [p.name for p in user.favorites_people],
+        'favorite_planets': [p.name for p in user.favorites_planets],
+    })
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    user = User.query.get_or_404(request.args.get('user_id'))  # Assuming user_id is passed as a query param
+    person = People.query.get_or_404(people_id)
+    if person not in user.favorites_people:
+        user.favorites_people.append(person)
+        db.session.commit()
+    return jsonify({'message': 'Favorite added.'})
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user = User.query.get_or_404(request.args.get('user_id'))  # Assuming user_id is passed as a query param
+    planet = Planet.query.get_or_404(planet_id)
+    if planet not in user.favorites_planets:
+        user.favorites_planets.append (planet_id)
+        db.session.commit()
+    return jsonify({'message': 'Favorite added.'})
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def remove_favorite_people(people_id):
+    user = User.query.get_or_404(request.args.get('user_id'))  # Assuming user_id is passed as a query param
+    person = People.query.get_or_404(people_id)
+    if person in user.favorites_people:
+        user.favorites_people.remove(person)
+        db.session.commit()
+    return jsonify({'message': 'Favorite removed.'})
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def remove_favorite_planet(planet_id):
+    user = User.query.get_or_404(request.args.get('user_id'))  # Assuming user_id is passed as a query param
+    planet = Planet.query.get_or_404(planet_id)
+    if planet in user.favorites_planets:
+        user.favorites_planets.remove(planet)
+        db.session.commit()
+    return jsonify({'message': 'Favorite removed.'})
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(debug=True)
--- src/models.py
+++ src/models.py
@@ -4,16 +4,16 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    favorites_people = db.relationship('People', backref='fan', lazy='dynamic')
+    favorites_planets = db.relationship('Planet', backref='fan', lazy='dynamic')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
